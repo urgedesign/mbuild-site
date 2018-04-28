58ddfa5df81a987de784ea08f6ca0dc47fd0a628
Glen Honeybone <glen@urgedesign.co.nz>
2018-04-28 18:25:04 +1200

    added details

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 const IndexPage = () => (
   <div>
-    <h1>Christchurch Builders</h1>
-    <img src="./static/logo.png" />
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1 className="title">Christchurch Builders</h1>
+    <img src="./static/logo.svg" />
+    <h2>Muir Builders</h2>
+    <p>Neil Muir</p>
+    <p>
+      P.&nbsp;<a href="te;:+64277600060">027 760 0060</a>
+    </p>
+    <p>
+      E.&nbsp;<a href="mailto:neil@mbuild.co.nz">neil@mbuild.co.nz</a>
+    </p>
   </div>
 )
 
