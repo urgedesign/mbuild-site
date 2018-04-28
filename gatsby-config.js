5ad9a7390a549a5fb71b4fc18e56514201db69af
Glen Honeybone <glen@urgedesign.co.nz>
2018-04-28 15:50:08 +1200

    initial dev

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'M.Build – Christchurch Builders',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
+    <h1>Christchurch Builders</h1>
+    <img src="./static/logo.png" />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
 
